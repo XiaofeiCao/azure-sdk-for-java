3cdc6ec785979c5e2f44546cc3c4c911b50777fd
XiaofeiCao <xiaofeicao@microsoft.com>
2023-09-11 15:51:03 +0800

    main

--- sdk/resourcemanager/tools/javadoc/src/main/java/com/azure/resourcemanager/tools/javadoc/Main.java
+++ sdk/resourcemanager/tools/javadoc/src/main/java/com/azure/resourcemanager/tools/javadoc/Main.java
@@ -4,8 +4,6 @@
 package com.azure.resourcemanager.tools.javadoc;
 
 public class Main {
-    private final static String BREAKING_CHANGE_TITLE = "### Breaking Changes\n\n";
-    private final static String NEW_FEATURE_TITLE = "### Features Added\n\n";
 
     public static void main(String[] args) throws Exception {
     }
