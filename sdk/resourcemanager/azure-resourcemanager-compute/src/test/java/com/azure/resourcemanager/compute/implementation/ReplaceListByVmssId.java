d7d3703fc50863c96db8001b64667254188e85c4
XiaofeiCao <xiaofeicao@microsoft.com>
2023-09-19 17:25:51 +0800

    add java parser

---
+++ sdk/resourcemanager/azure-resourcemanager-compute/src/test/java/com/azure/resourcemanager/compute/implementation/ReplaceListByVmssId.java
@@ -0,0 +1,16 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+package com.azure.resourcemanager.compute.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class ReplaceListByVmssId {
+    @Test
+    public void replaceListByResourceGroupAndNextLink() throws Exception {
+        File file = new File("");
+        System.out.println(file.getAbsolutePath());
+    }
+}
