91aa398e5bb651ca58fc0c3eacc1b349e8adf29c
XiaofeiCao <xiaofeicao@microsoft.com>
2024-03-08 14:45:55 +0800

    add policy, enable netty log

--- sdk/resourcemanager/azure-resourcemanager/src/test/java/com/azure/resourcemanager/vmware/VMWareTests.java
+++ sdk/resourcemanager/azure-resourcemanager/src/test/java/com/azure/resourcemanager/vmware/VMWareTests.java
@@ -3,12 +3,49 @@
 
 package com.azure.resourcemanager.vmware;
 
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.http.HttpClient;
+import com.azure.core.http.HttpPipeline;
+import com.azure.core.http.policy.HttpLogOptions;
+import com.azure.core.http.policy.HttpPipelinePolicy;
+import com.azure.core.http.policy.RetryPolicy;
+import com.azure.core.management.profile.AzureProfile;
 import com.azure.resourcemanager.AzureResourceManagerTests;
+import com.azure.resourcemanager.resources.fluentcore.utils.HttpPipelineProvider;
+import com.azure.resourcemanager.resources.models.ResourceGroup;
 import org.junit.jupiter.api.Test;
+
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class VMWareTests extends AzureResourceManagerTests {
     @Test
     public void testVmware() {
+        azureResourceManager.resourceGroups().list().forEach(new Consumer<ResourceGroup>() {
+            @Override
+            public void accept(ResourceGroup resourceGroup) {
 
+            }
+        });
+    }
+
+    @Override
+    protected HttpPipeline buildHttpPipeline(
+        TokenCredential credential,
+        AzureProfile profile,
+        HttpLogOptions httpLogOptions,
+        List<HttpPipelinePolicy> policies,
+        HttpClient httpClient) {
+        policies.add(new DynamicThrottlePolicy(clientIdFromFile()));
+        return HttpPipelineProvider.buildHttpPipeline(
+            credential,
+            profile,
+            null,
+            httpLogOptions,
+            null,
+            new RetryPolicy("Retry-After", ChronoUnit.SECONDS),
+            policies,
+            httpClient);
     }
 }
